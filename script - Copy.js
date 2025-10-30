56b838b3fe4e06df62916304ec0510df08317777
vipuljadhav018-gif <vipuljadhav018@gmail.com>
2025-10-30 21:49:29 +0530

    Add files via upload

---
+++ script - Copy.js
@@ -0,0 +1,11 @@
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+  cursor.style.left = e.pageX + 'px';
+  cursor.style.top = e.pageY + 'px';
+});
+
+document.querySelectorAll('a, button').forEach(el => {
+  el.addEventListener('mouseenter', () => cursor.style.transform = 'scale(1.5)');
+  el.addEventListener('mouseleave', () => cursor.style.transform = 'scale(1)');
+});
